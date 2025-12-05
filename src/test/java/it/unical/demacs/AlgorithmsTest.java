fb33de3cde91ea4be3c4ee427a61b4b97138af9c
AntonioFalace01 <falaceantonio290@gmail.com>
2025-12-05 11:13:18 +0100

    Initialize Algorithms instance in test setup

--- src/test/java/it/unical/demacs/AlgorithmsTest.java
+++ src/test/java/it/unical/demacs/AlgorithmsTest.java
@@ -1,9 +1,14 @@
 package it.unical.demacs;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class AlgorithmsTest {
 
     private Algorithms alg;
 
     @BeforeEach
+    public void setUp() {
+        alg = new Algorithms();
+    }
 
 }
